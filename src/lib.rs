87d65a4f98bad92f360f8ff42e2c198dfa33db55
Olivier Lemoine <olivier.lemoine@432technologies.com>
2024-04-29 11:29:28 -0400

    Add a bit of documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+/*!
+
+# System Independent Timer
+
+Async timer that works on wasm (with gloo) and linux/macos/windows (with tokio)
+
+```rust
+use std::time::Duration;
+
+async {
+    sint::Timeout::new(Duration::from_millis(500)).await;
+
+    //Or equivalent
+    sint::sleep(Duration::from_millis(500)).await;
+};
+```
+
+*/
+
 use std::{
     future::Future,
     pin::Pin,
